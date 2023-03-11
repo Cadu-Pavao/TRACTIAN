0566855c13f83b3991f44e484554353676a36c1f
cadu pavao <cadupavao@gmail.com>
2023-03-10 22:00:15 -0300

    chore: upload assets

--- tractian-landing/src/assets/index.js
+++ tractian-landing/src/assets/index.js
@@ -1,5 +1,8 @@
+import background from "./background.svg";
+import logo from "./logo.svg";
+import sensor from "./sensor_1.webp";
+import sensorDiagonal from "./sensor_1.png";
 import stamp from "./stamp.svg";
 import star from "./star.svg";
-import sensor from "./sensor_1.webp";
 
-export { stamp, star, sensor };
+export { background, logo, sensor, sensorDiagonal, stamp, star };
