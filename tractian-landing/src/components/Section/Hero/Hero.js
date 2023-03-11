25ef9908e44cbd6bcdaa530d63da9cb2fcc48df3
cadu pavao <cadupavao@gmail.com>
2023-03-10 22:02:05 -0300

    chore: insert new components in hero section

---
+++ tractian-landing/src/components/Section/Hero/Hero.js
@@ -0,0 +1,26 @@
+import styles from "./Hero.module.css";
+import { Button } from "../../UI/Button/Button";
+import { HeroImage } from "./HeroImage";
+
+export function Hero() {
+  return (
+    <>
+      <div className={styles.heroWrapper}>
+        <div className={styles.text}>
+          <p>Startup que mais cresce na América-Latina</p>
+          <h1>
+            Monitoramento online e Gestão de Ativos em{" "}
+            <span className={styles.textHighlight}>um só lugar</span>
+          </h1>
+          <p>
+            O futuro é agora! Evite falhas em sua máquinas e deixe o tempo de
+            inatividade no passado com o melhor sistema preditivo do mercado.
+          </p>
+
+          <Button>Demonstração</Button>
+        </div>
+        <HeroImage />
+      </div>
+    </>
+  );
+}
--- tractian-landing/src/pages/index.js
+++ tractian-landing/src/pages/index.js
@@ -1,34 +1,20 @@
 import Head from "next/head";
-import { Stamp } from "@/components/Stamp/Stamp";
-import Image from "next/image";
-import { sensor } from "@/assets";
 import styles from "@/styles/Home.module.css";
+import { Hero } from "@/components/Section/Hero/Hero";
+import { Navbar } from "@/components/UI/Navbar/Navbar";
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Create Next App</title>
+        <title>TRACTIAN</title>
         <meta name="description" content="Landing page for TRACTIAN" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <h2>Criando componentes:</h2>
-        <p>
-          Por hora esta é uma sessão apenas para testar os componentes enquanto
-          os crio.
-        </p>
-        <Image
-          src={sensor}
-          alt="Nosso sensor"
-          width={480}
-          height={964}
-          priority
-          unoptimized
-        />
-
-        <Stamp></Stamp>
+        <Navbar />
+        <Hero />
       </main>
     </>
   );
