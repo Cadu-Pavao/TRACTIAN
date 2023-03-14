3d4aa642ccaf59bf2973b7f9964aac02cfea81cd
cadu pavao <cadupavao@gmail.com>
2023-03-14 18:41:29 -0300

    feat: hero section animation

--- tractian-landing/src/components/Section/Hero/Hero.js
+++ tractian-landing/src/components/Section/Hero/Hero.js
@@ -3,12 +3,32 @@ import { Button } from "../../UI/Button/Button";
 import { HeroImage } from "./HeroImage";
 import { google_rate, capterra } from "@/assets";
 import Image from "next/image";
+import { useLayoutEffect, useRef } from "react";
+import gsap from "gsap";
+import CustomEase from "gsap/dist/CustomEase";
+
+gsap.registerPlugin(CustomEase);
 
 export function Hero() {
+  const heroContent = useRef();
+  const root = useRef();
+
+  useLayoutEffect(() => {
+    let context = gsap.context(() => {
+      gsap.from(heroContent.current, {
+        opacity: 0,
+        y: 100,
+        duration: 1,
+        ease: CustomEase.create("custom", "M0,0 C0.772,0 0.34,1 1,1 "),
+      });
+    }, root);
+    return () => context.revert();
+  }, []);
+
   return (
     <>
-      <div className={styles.heroWrapper}>
-        <div className={styles.content}>
+      <div ref={root} className={styles.heroWrapper}>
+        <div ref={heroContent} className={styles.content}>
           <div className={styles.rewards}>
             <Image src={capterra} alt="" />
             <Image src={google_rate} alt="" />
