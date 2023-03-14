b88e1d3065e2b7cd8be7412ce205f103b90a415a
cadu pavao <cadupavao@gmail.com>
2023-03-14 16:38:35 -0300

    feat: include all assets

--- tractian-landing/src/assets/index.js
+++ tractian-landing/src/assets/index.js
@@ -1,8 +1,41 @@
+import apple from "./selo_apple.png";
 import background from "./background.svg";
+import barbieri from "./barbieri.jpg";
+import bhs from "./bhs.jpeg";
+import celphone from "./celphone.png";
+import capterra from "./capterra.svg";
+import google from "./selo_google.png";
+import google_rate from "./google_rate.svg";
 import logo from "./logo.svg";
+import logo_white from "./logo_white.svg";
 import sensor from "./sensor_1.webp";
 import sensorDiagonal from "./sensor_1.png";
 import stamp from "./stamp.svg";
 import star from "./star.svg";
+import suspensys from "./suspensys.webp";
+import tractian_1 from "./Tracian_01.png";
+import tractian_2 from "./Tracian_02.png";
+import tractian_3 from "./Tracian_03.png";
+import tractian_4 from "./Tracian_04.png";
 
-export { background, logo, sensor, sensorDiagonal, stamp, star };
+export {
+  apple,
+  background,
+  barbieri,
+  bhs,
+  capterra,
+  celphone,
+  google,
+  google_rate,
+  logo,
+  logo_white,
+  sensor,
+  sensorDiagonal,
+  stamp,
+  star,
+  suspensys,
+  tractian_1,
+  tractian_2,
+  tractian_3,
+  tractian_4,
+};
